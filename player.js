cd83bf92f9338191b89c9288ee51141321d31ba3
Tia-11 <143324169+Tia-11@users.noreply.github.com>
2023-10-19 16:09:39 +0200

    mattia.4

--- player.js
+++ player.js
@@ -1,19 +1,67 @@
 const playPauseButton = document.getElementById("play-pause-button");
+const audio = new Audio();
+const progressBar = document.getElementById("progress-bar");
+const timePassed = document.getElementById("time-passed");
+const timeTotal = document.getElementById("time-total");
+const likeIcon = document.getElementById("like-icon");
+const likeTooltip = document.getElementById("like-tooltip");
+const likeTooltip2 = document.getElementById("like-tooltip2");
+const shaker = document.querySelector(".shacker");
+const clock = document.querySelector("#timePassed");
+const shuffle = document.querySelector(".bi-shuffle");
+const rewind = document.querySelector(".fa-undo-alt");
+const back = document.querySelector(".fa-step-backward");
+const volume = document.getElementById("volume-bar");
+let isPlaying = false;
+let currentTime = 0;
 
-playPauseButton.addEventListener("click", function () {
-  if (playPauseButton.classList.contains("bi-play-circle-fill")) {
+fetch("https://striveschool-api.herokuapp.com/api/deezer/album/75621062")
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new error("errore");
+    }
+  })
+  .then((album) => {
+    console.log(album);
+    const titolo = document.querySelector("#player .ms-3 h6");
+    titolo.innerText = album.tracks.data[0].title;
+    const artista = document.querySelector("#player .ms-3 p");
+    artista.innerText = album.tracks.data[0].artist.name;
+    const immagine = document.querySelector("#player .img1 img");
+    immagine.src = album.tracks.data[0].album.cover;
+    const clockStart = document.querySelector("#timeStart");
+    clockStart.innerText = `0:${album.tracks.data[0].duration - 1}`;
+    playPauseButton.addEventListener("click", function () {
+      if (!isPlaying) {
+        if (currentTime === 0) {
+          audio.src = album.tracks.data[0].preview;
+        }
+        audio.currentTime = currentTime;
+        audio.play();
+        isPlaying = true;
+      } else {
+        audio.pause();
+        currentTime = audio.currentTime;
+        isPlaying = false;
+      }
+      updatePlayButtonState();
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+function updatePlayButtonState() {
+  if (isPlaying) {
     playPauseButton.classList.remove("bi-play-circle-fill");
     playPauseButton.classList.add("bi-pause-circle-fill");
   } else {
     playPauseButton.classList.remove("bi-pause-circle-fill");
     playPauseButton.classList.add("bi-play-circle-fill");
   }
-});
-
-const likeIcon = document.getElementById("like-icon");
-const likeTooltip = document.getElementById("like-tooltip");
-const likeTooltip2 = document.getElementById("like-tooltip2");
-const shaker = document.querySelector(".shacker");
+}
 
 likeIcon.addEventListener("click", function () {
   likeTooltip2.style.display = "none";
@@ -37,7 +85,7 @@ likeIcon.addEventListener("click", function () {
   }
   setTimeout(function () {
     likeTooltip2.style.display = "none";
-  }, 1000);
+  }, 2000);
 });
 
 likeIcon.addEventListener("mouseover", function () {
@@ -51,4 +99,71 @@ likeIcon.addEventListener("mouseover", function () {
 
 likeIcon.addEventListener("mouseout", function () {
   likeTooltip.style.display = "none";
+});
+
+shuffle.addEventListener("click", function () {
+  rewind.classList.add("hover");
+  if (shuffle.classList.contains("text-success")) {
+    shuffle.classList.remove("text-success");
+    shuffle.classList.add("hover");
+  } else {
+    shuffle.classList.add("text-success");
+    rewind.classList.remove("text-success");
+    shuffle.classList.remove("hover");
+  }
+});
+
+rewind.addEventListener("click", function () {
+  shuffle.classList.add("hover");
+  if (rewind.classList.contains("text-success")) {
+    rewind.classList.remove("text-success");
+    rewind.classList.add("hover");
+  } else {
+    rewind.classList.add("text-success");
+    shuffle.classList.remove("text-success");
+    rewind.classList.remove("hover");
+  }
+});
+
+back.addEventListener("click", function () {
+  currentTime = 0;
+  audio.currentTime = currentTime;
+  clock.innerText = "0:00";
+  if (!isPlaying) {
+    playPauseButton.click();
+  }
+});
+
+audio.addEventListener("ended", function () {
+  if (rewind.classList.contains("text-success")) {
+    audio.currentTime = 0;
+    audio.play();
+  } else {
+    if (isPlaying) {
+      playPauseButton.classList.remove("bi-pause-circle-fill");
+      playPauseButton.classList.add("bi-play-circle-fill");
+    }
+  }
+  clock.innerText = "0:00";
+});
+
+audio.addEventListener("timeupdate", function () {
+  const currentTime = audio.currentTime;
+  const duration = audio.duration;
+  const progressBar = document.getElementById("progress-bar");
+
+  const percentCompleted = (currentTime / duration) * 100;
+
+  progressBar.style.width = percentCompleted + "%";
+
+  const minutes = Math.floor(currentTime / 60);
+  const seconds = Math.floor(currentTime % 60);
+  const formattedTime = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  document.getElementById("timePassed").innerText = formattedTime;
+});
+
+audio.volume = volume.value / 100;
+
+volume.addEventListener("input", function () {
+  audio.volume = volume.value / 100;
 });
